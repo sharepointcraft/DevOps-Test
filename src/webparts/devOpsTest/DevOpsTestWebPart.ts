c90827d2244d5996a80785a2c2f1cafdfcdf70f2
SharePoint Craft <sharepointcraft@gmail.com>
2024-08-01 16:03:17 +0530

    Test2

--- src/webparts/devOpsTest/DevOpsTestWebPart.ts
+++ src/webparts/devOpsTest/DevOpsTestWebPart.ts
@@ -29,7 +29,7 @@ export default class DevOpsTestWebPart extends BaseClientSideWebPart<IDevOpsTest
         <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
       </div>
       <div>
-        <h3>Welcome to SharePoint Framework!</h3>
+        <h3>Welcome to SharePoint Framework!!</h3>
         <p>
         The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
         </p>
